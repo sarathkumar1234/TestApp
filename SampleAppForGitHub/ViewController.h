c4ef2502882cfbc452266797b3eaa136dd9fa8f0
shiva <shivakumargouda.patil@mobinius.com>
2015-12-17 16:35:57 +0530

    Initial Commit

---
+++ SampleAppForGitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleAppForGitHub
+//
+//  Created by Mobinius Mac on 17/12/15.
+//  Copyright © 2015 mobinius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SampleAppForGitHub/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SampleAppForGitHub
+//
+//  Created by Mobinius Mac on 17/12/15.
+//  Copyright © 2015 mobinius. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
